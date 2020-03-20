f3442019354364388f38c08ddbf876ded89f3bcf
Remim7 <1197097889@qq.com>
2020-03-20 18:23:48 +0800

    change omni to orangefox

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,33 @@
-add_lunch_combo omni_nash-eng
-add_lunch_combo omni_nash-userdebug
+#
+# Copyright (C) 2019-2020 OrangeFox Recovery Project
+#
+# This software is licensed under the terms of the GNU General Public
+# License version 2, as published by the Free Software Foundation, and
+# may be copied, distributed, and modified under those terms.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# See <http://www.gnu.org/licenses/>.
+#
+# Please maintain this if you use this script or any part of it
+#
+DEVICE="nash"
+if [ "$1" = "$DEVICE" -o "$FOX_BUILD_DEVICE" = "$DEVICE" ]; then
+	export FOX_DELETE_AROMAFM="1"
+        export PLATFORM_VERSION="10.0.0"
+	export OF_AB_DEVICE=1
+	export OF_USE_MAGISKBOOT=1
+	export OF_USE_NEW_MAGISKBOOT=1
+	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+	
+	# let's see what are our build VARs
+	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+  	fi
 
+	add_lunch_combo omni_"$DEVICE"-eng
+fi
